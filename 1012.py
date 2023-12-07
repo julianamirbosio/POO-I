8b757cf13df7aa3e0bd393d002f106e4111963e2
julianamirbosio <128743881+julianamirbosio@users.noreply.github.com>
2023-12-07 13:06:19 -0300

    Área

---
+++ 1012.py
@@ -0,0 +1,14 @@
+A, B, C = input().split()
+A = float(A)
+B = float(B)
+C = float(C)
+TRI = (A*C)/2
+CIR = 3.14159*(C**2)
+TRA = (A+B)*C/2
+QUA = B**2
+RET = A*B
+print("TRIANGULO: ""%.3f" % (TRI))
+print("CIRCULO: ""%.3f" % (CIR))
+print("TRAPEZIO: ""%.3f" % (TRA))
+print("QUADRADO: ""%.3f" % (QUA))
+print("RETANGULO: ""%.3f" % (RET))
